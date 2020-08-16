3123ce7e02e47329a239cd7e738fe37dd70ff5fa
zhai <zhai@tn.com>
2020-08-16 10:09:09 +0800

    aaa

--- src/Jenkinsfile/projectA-stage.groovy
+++ src/Jenkinsfile/projectA-stage.groovy
@@ -6,10 +6,12 @@ pipeline{
 	agent any
 	stages{
 		stage("Hello Pipeline") {
-			script {
-				println "Hello Pipeline!"
-				println env.JOB_NAME
-				println env.BUILD_NUMBER
+			steps {
+			    script {
+					println "Hello Pipeline!"
+					println env.JOB_NAME
+					println env.BUILD_NUMBER
+				}
 			}
 		}
 	}
